eeab87f541fcebfedbe1ce205494e7c40d0e4195
Umair <umairswishtag@gmail.com>
2026-04-28 16:11:18 +0500

    url fix

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,4 @@ use App\Http\Controllers\Exercise1TierPricing;
 use App\Http\Controllers\Exercise2TierPricing;
 
 Route::post('/exercise-1-artwork-version', [App\Http\Controllers\Exercise1ArtworkVersion::class, 'store']);
-Route::post('/exercise2-tier-pricing', [App\Http\Controllers\Exercise2TierPricing::class, 'getPrice']);
+Route::post('/exercise-2-tier-pricing', [App\Http\Controllers\Exercise2TierPricing::class, 'getPrice']);
