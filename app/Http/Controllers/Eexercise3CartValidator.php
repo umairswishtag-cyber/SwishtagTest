d5131cd10dba1c6e8eac4ffb3cddc0d25faae3b2
Umair <umairswishtag@gmail.com>
2026-04-29 16:01:37 +0500

    Eexercise 3 Cart Validator

---
+++ app/Http/Controllers/Eexercise3CartValidator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Eexercise3CartValidator extends Controller
+ {
+    public function CartIntegrityValidator(Request $request){
+         $items = $request->input('input', []);
+    if (!is_array($items)) {
+        return response()->json([
+            'success' => false,
+            'data' => null,
+            'error' => 'Invalid input'
+        ]);
+    }
+    $invalidItems = collect($items)
+        ->filter(function ($item) {
+            return isset($item['id'], $item['required'], $item['done']) &&
+                filter_var($item['required'], FILTER_VALIDATE_BOOLEAN) &&
+                !filter_var($item['done'], FILTER_VALIDATE_BOOLEAN);
+        })
+        ->pluck('id')
+        ->values()
+        ->toArray();
+    return response()->json([
+        'success' => true,
+        'data' => [
+            'valid' => empty($invalidItems),
+            'invalid_items' => $invalidItems
+        ],
+        'error' => null
+    ]);
+}
+ 
+ 
+
+}
+ 
+ 
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Exercise1TierPricing;
 use App\Http\Controllers\Exercise2TierPricing;
+use App\Http\Controllers\Eexercise3CartValidator;
+
+Route::post('/cart-integrity-validator', [Eexercise3CartValidator::class, 'CartIntegrityValidator']);
 
 Route::post('/exercise-1-artwork-version', [App\Http\Controllers\Exercise1ArtworkVersion::class, 'store']);
 Route::post('/exercise-2-tier-pricing', [App\Http\Controllers\Exercise2TierPricing::class, 'getPrice']);
