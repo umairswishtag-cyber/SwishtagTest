c416762b14c98019e3172147596591e2cc578353
Umair <umairswishtag@gmail.com>
2026-04-30 20:39:46 +0500

    fixed

--- app/Http/Controllers/Exercise4VendorAllocation.php
+++ app/Http/Controllers/Exercise4VendorAllocation.php
@@ -6,41 +6,42 @@ use Illuminate\Http\Request;
 
 class Exercise4VendorAllocation extends Controller
 {
-   public function allocate(Request $request){
-    $input = $request->input('input', []);
-    if (!isset($input['order_qty']) || !isset($input['vendors'])) {
-        return response()->json([
-            'success' => false,
-            'data' => null,
-            'error' => 'Not Valid'
+    public function allocate(Request $request)
+    {
+        $input = $request->input('input', []);
+        if (!isset($input['order_qty']) || !isset($input['vendors'])) {
+            return response()->json([
+                'success' => false,
+                'data' => null,
+                'error' => 'Not Valid'
             ]);
-            }
-            
-            $remaining = $input['order_qty']; 
-            $vendors = $input['vendors'];
-            $result = [];
+        }
 
-            // dd($request->input('input.vendors'));
-            
-            foreach ($vendors as $vendor) {
-                
-                if ($remaining <= 0) break;
-                $allocate = min($remaining, $vendor['stock']);
+        $remaining = $input['order_qty'];
+        $vendors = $input['vendors'];
+        $result = [];
+
+        // dd($request->input('input.vendors'));
+
+        foreach ($vendors as $vendor) {
+
+            if ($remaining <= 0) break;
+            $allocate = min($remaining, $vendor['stock']);
+            if ($vendor['stock'] > 0) {
                 $result[] = [
                     'vendor_id' => $vendor['id'],
                     'allocated' => $allocate
-                    ];
-                    $remaining -= $allocate;
-                    }
-                    
-                return response()->json([
-        'success' => true,
-        'data' => $result,
+                ];
+                $remaining -= $allocate;
+            }
+        }
 
-        'error' => null
-    
+        return response()->json([
+            'success' => true,
+            'data' => $result,
+
+            'error' => null
+
         ]);
-
-}
-
+    }
 }
