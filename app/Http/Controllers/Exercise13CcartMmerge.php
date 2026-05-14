36e1d0af7dd88a1ab807ae3143c15676c65dd15d
Umair <umairswishtag@gmail.com>
2026-05-14 17:18:07 +0500

    exercise-13-cart-merge

---
+++ app/Http/Controllers/Exercise13CcartMmerge.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Exercise13CcartMmerge extends Controller
+{
+    public function mergeCart(Request $request)
+
+    {
+        $input = $request->input('input', []);
+        if (!isset($input['guest']) || !isset($input['user'])) {
+            return response()->json([
+                "success" => false,
+                "data" => null,
+                "error" => "Invalid input"
+            ]);
+        }
+        $guestCart = $input['guest'];
+        $userCart = $input['user'];
+        $merged = [];
+        foreach ($userCart as $item) {
+            $merged[$item['id']] = [
+                "id" => $item['id'],
+                "qty" => $item['qty']
+            ];
+        }
+        foreach ($guestCart as $item) {
+
+           if (isset($merged[$item['id']])) {
+                $merged[$item['id']]['qty'] += $item['qty'];
+            } else {
+                $merged[$item['id']] = [
+                    "id" => $item['id'],
+                    "qty" => $item['qty']
+                ];
+            }
+        }
+        return response()->json([
+            "success" => true,
+            "data" => array_values($merged),
+            "error" => null
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\Exercise9Webhook;
 use App\Http\Controllers\Exercise10QuoteEexpiry;
 use App\Http\Controllers\Ercise11ProductVisibility;
 use App\Http\Controllers\Exercise12BundlePricing;
+use App\Http\Controllers\Exercise13CcartMmerge; 
 
 Route::post('/exercise-1-artwork-version', [App\Http\Controllers\Exercise1ArtworkVersion::class, 'store']);
 Route::post('/exercise-2-tier-pricing', [App\Http\Controllers\Exercise2TierPricing::class, 'getPrice']);
@@ -26,6 +27,7 @@ Route::post('/exercise-9-webhook', [Exercise9Webhook::class, 'handleWebhook']);
 Route::post('/exercise-10-quote-expiry', [Exercise10QuoteEexpiry::class, 'checkQuoteExpiry']); 
 Route::post('/exercise-11-product-visibility', [Ercise11ProductVisibility::class, 'checkProductVisibility']);
 Route::post('/exercise-12-bundle-pricing', [Exercise12BundlePricing::class, 'bundlePricing']);
+Route::post('/exercise-13-cart-merge', [Exercise13CcartMmerge::class, 'mergeCart']);
  
 
  
